8fd1cb45920690d544c20504f187f3c1f23d2105
Joseph Woo <jwoo@Josephs-Air.home>
2016-06-08 13:03:55 -0400

    added files

---
+++ jWoo.js
@@ -0,0 +1,13 @@
+//doesn't work rofl
+/*function active(){
+	var url = window.location.href;
+	switch (url){
+		case "jWooExp.html":
+			document.getElementById('menuExp').style.class="active";
+			break;
+		case "jWooPortfolio.html":
+			document.getElementById('menuPortfolio').style.class="active";
+		default:
+			//list items should not be "active"
+	}
+}*/
