4c6fc1408eb25d69ef489279fde91f34700696ab
Joseph Woo <jwoo@Josephs-Air.home>
2016-11-17 15:02:32 -0500

    updated libs file "$connection" line

--- libs.php
+++ libs.php
@@ -3,7 +3,7 @@
 
 	function connect(){
 		$database = "jWoo";
-		$connection = new mysqli("192.168.1.94", "root", "1Asxdcfv", "mysql");
+		$connection = new mysqli("192.168.1.94", "root", "1Asxdcfv", "jWoo");
 		if(mysqli_connect_errno()){
 			echo("Could not connect to MySQL".mysqli_connect_error());
 		}
